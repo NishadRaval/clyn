a252963a78bee8b8492e57a78c62b4462f6da4d5
NishadRaval <nishadsraval@gmail.com>
2025-10-21 14:48:00 +0530

    Fix CORS origin for new Vercel deployment URL

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     'http://localhost:5173',       // For your local testing
-    'https://leval-1.vercel.app'  // For your live website
+    'https://leval1.vercel.app'  // For your live website
   ],
   credentials: true,
 }));
